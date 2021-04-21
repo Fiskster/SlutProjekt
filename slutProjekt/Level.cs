b3ce87ae0f5a2c1108edd57c827ad8634b1436ba
Axel <axel.lilja@elev.ga.ntig.se>
2021-04-21 11:07:13 +0200

    La till en Queue för en infinite runner

---
+++ slutProjekt/Level.cs
@@ -0,0 +1,23 @@
+using System;
+using Raylib_cs;
+using System.Collections.Generic;
+
+namespace slutProjekt
+{
+    public class Level : GameObject
+    {
+        Queue<GameObject> tileQueue = new Queue<GameObject>();
+
+
+        public override void Update() 
+        {
+
+            
+
+
+
+        }
+
+
+    }
+}
